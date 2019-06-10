495f69c0330bb9b75f66887eb55bdef80cab0e33
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-06-10 22:00:42 +0200

    refactored js scroll effect animation to be more dynamic

--- app/js/folder-animation.js
+++ app/js/folder-animation.js
@@ -2,7 +2,13 @@ const folderCover = document.querySelector('.folder-cover');
 
 const startFolderAnimation = () => {
     const animationContainer = document.querySelector('.folder');
-    const sections = document.querySelectorAll('.project');
+    let sections = document.querySelectorAll('.project');
+
+    // extand section for the animation
+    extandWrapperHeight(sections);
+
+    // fade in the elements
+    fadeInSections(sections);
 
     // Dynamically establish how low the folder should be moved
     animationContainer.style.transform = `translateY(${calculateSectionsHeight(sections)})`;
@@ -13,19 +19,18 @@ const startFolderAnimation = () => {
     // dynamically establish how long should the folder open/close animation last
     folderCover.style.animation = `open-folder ${1 * sections.length}s ease-out forwards 0s`;
 
-
-    fadeInSections(sections);
-
   }
 
+// Extand the wrapper height
+const extandWrapperHeight = (sections) => {
+  sections.forEach((section)=> {
+    section.style.display ="block";
+  })
+}
 
 //   dynamically calculate the height of elements and return this value as string with px
   const calculateSectionsHeight = (sections)=> {
-    let heightPx = 0;
-    sections.forEach((section)=> {
-        heightPx += section.offsetHeight;
-    })
-
+    let heightPx = document.querySelector('.projects-wrapper').offsetHeight;
     return (heightPx + 'px');
   }
 
@@ -33,12 +38,37 @@ const startFolderAnimation = () => {
 // dynamically assign opcaity delay depanding of number of those
 const fadeInSections = (sections) => {
     sections.forEach((section, index)=> {
-        section.style.animation =`show-section 1s ease-in forwards ${index * 0.5}s`;
+      // for each 300px add half a second
+        let sectionHeightFallingTime = (section.offsetHeight/1000) * 0.5;
+        section.style.animation =`show-section 1s ease-in forwards ${index * 0.5 + sectionHeightFallingTime}s`;
+        
     })
 }
 
 
-  // liten for scroll position to trigger animation
-  folderCover.addEventListener('click', (event)=>{
-    startFolderAnimation();
-  })
+  // Check if folder is in view
+  const isInView = () =>{
+      const folder = document.querySelector('.folder');
+      const folderHeight = folder.clientHeight;
+      const scrollY = window.scrollY || window.pageYOffset
+      const windowHeight = window.innerHeight;
+      const scrollPosition = scrollY + windowHeight;
+      const folderPosition = folder.getBoundingClientRect().top + scrollY + folderHeight;
+
+      return(scrollPosition >= folderPosition)
+ 
+  }
+
+
+  const startAnimationOnScroll = ()=> {
+    window.addEventListener('scroll', ()=> {
+
+      if(isInView()){
+        startFolderAnimation()
+      }
+    })
+
+  }
+
+  startAnimationOnScroll()
+
