ba75331c6e5aa46b8c41a58d50c2c9f964572464
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-06-13 22:35:55 +0200

    corrected menu navigation

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,9 +1,6 @@
-const btnBurger = document.getElementById('nav-top-burger');
-btnBurger.addEventListener('click', ()=> {
-    
+const toggleMobileNav = ()=> {
     const menu = document.getElementById('nav-menu');
     const menuItems = document.querySelectorAll('.menu .menu__item');
-    let fadeInTime
     menu.classList.toggle('toggle-mobile-nav');
     btnBurger.classList.toggle('menu-mobile__burger--toggled');
 
@@ -14,5 +11,20 @@ btnBurger.addEventListener('click', ()=> {
             menuItem.style.animation = `linkFading 0.5s ease forwards ${index /7 + 1}s`;
         }
     });
-    
+}
+
+const btnBurger = document.getElementById('nav-top-burger');
+btnBurger.addEventListener('click', ()=> { 
+    toggleMobileNav()
 })
+
+const menuButtons = document.querySelectorAll('.menu__item');
+menuButtons.forEach((button)=>{
+    button.addEventListener('click', ()=> {
+        toggleMobileNav()
+    })
+})
+
+
+
+
