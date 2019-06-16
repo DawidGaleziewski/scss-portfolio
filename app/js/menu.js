851613b64932089cb56097495032b4a3029cb94a
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-06-16 22:05:43 +0200

    fixed js animation making it trigger only on mobile

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,30 +1,35 @@
-const toggleMobileNav = ()=> {
-    const menu = document.getElementById('nav-menu');
-    const menuItems = document.querySelectorAll('.menu .menu__item');
-    menu.classList.toggle('toggle-mobile-nav');
-    btnBurger.classList.toggle('menu-mobile__burger--toggled');
+// check if device is mobile on load
+const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
 
-    menuItems.forEach((menuItem,index)=> {
-        if(menuItem.style.animation){
-            menuItem.style.animation= '';
-        } else{
-            menuItem.style.animation = `linkFading 0.5s ease forwards ${index /7 + 1}s`;
-        }
-    });
-}
+// If device is mobile run rest of the code
+if(isMobile) {
 
-const btnBurger = document.getElementById('nav-top-burger');
-btnBurger.addEventListener('click', ()=> { 
-    toggleMobileNav()
-})
+    const btnBurger = document.getElementById('nav-top-burger');
+    const menuButtons = document.querySelectorAll('.menu__item');
 
-const menuButtons = document.querySelectorAll('.menu__item');
-menuButtons.forEach((button)=>{
-    button.addEventListener('click', ()=> {
+    const toggleMobileNav = ()=> {
+        const menu = document.getElementById('nav-menu');
+        const menuItems = document.querySelectorAll('.menu .menu__item');
+        menu.classList.toggle('toggle-mobile-nav');
+        btnBurger.classList.toggle('menu-mobile__burger--toggled');
+
+        menuItems.forEach((menuItem,index)=> {
+            if(menuItem.style.animation){
+                menuItem.style.animation= '';
+            } else{
+                menuItem.style.animation = `linkFading 0.5s ease forwards ${index /7 + 1}s`;
+            }
+        });
+    }
+
+
+    btnBurger.addEventListener('click', ()=> { 
         toggleMobileNav()
     })
-})
-
-
-
-
+    
+    menuButtons.forEach((button)=>{
+        button.addEventListener('click', ()=> {
+            toggleMobileNav()
+        })
+    })
+}
