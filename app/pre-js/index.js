1d97af92f90707fb6d44874670e4a0bf96680e75
Dawid <dawidgalsino@vp.pl>
2019-12-03 13:28:40 +0100

    improved layout

--- app/pre-js/index.js
+++ app/pre-js/index.js
@@ -1,25 +1,18 @@
 // Main javascript file
-    //1. import modules
-    import Preloader from './modules/preloader';
-    import Menu from './modules/menu.js';
-    import FolderAnimation from './modules/folder-animation.js';
-    import PopupAnimation from './modules/popup-animation';
+//1. import modules
+import Preloader from './modules/preloader';
+import Menu from './modules/menu.js';
+import FolderAnimation from './modules/folder-animation.js';
+import PopupAnimation from './modules/popup-animation';
 
+//2. create objects
+const menu = new Menu();
+const preloader = new Preloader();
+const folderAnimation = new FolderAnimation();
+// const popupAnimation = new PopupAnimation;
 
-
-
-
-    //2. create objects
-    const menu = new Menu;
-    const preloader = new Preloader;
-    const folderAnimation = new FolderAnimation;
-    const popupAnimation = new PopupAnimation;
-
-  
-    
-    //3. start module
-    preloader.start();
-    menu.start();
-    folderAnimation.start();
-    popupAnimation.start()
-    
+//3. start module
+preloader.start();
+menu.start();
+folderAnimation.start();
+popupAnimation.start();
